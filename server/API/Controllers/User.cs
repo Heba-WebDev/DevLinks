c2dbdec85c27a213975819903a535e415993d0ae
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-14 18:03:04 +0300

    feat: added update profile controller

--- server/API/Controllers/User.cs
+++ server/API/Controllers/User.cs
@@ -1,49 +1,44 @@
+
 using Application.Contracts;
-using Application.Auth.Dtos;
+using Application.Dtos.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers;
-[Route("/api/v1/auth")]
+[Route("/api/v1/users")]
 [ApiController]
+
 public class User : ControllerBase
 {
     private readonly IUser _userRepo;
+
     public User(IUser user)
     {
         this._userRepo = user;
     }
-    [HttpPost("register")]
-    public async Task<IActionResult> Register(UserRegisterRequestDto dto)
-    {
-        var response = await _userRepo.UserRegisterAsync(dto);
 
+    [Authorize(Policy = "UserOrAdmin")]
+    [HttpPost("update-profile")]
+    public async Task<IActionResult> UpdateProfile(UserProfileRequestDto dto)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var userRole = User.FindFirstValue(ClaimTypes.Role);
+        if (userId != dto.Id && userRole != "Admin")
+        {
+            return Unauthorized("Unauthorized to perform this action");
+        }
+
+        var response = await _userRepo.UserProfileUpdateAsync(dto, userId!);
         if (!response.Flag)
         {
             return BadRequest(new { message = response.Message });
         }
-
         return Ok(new
         {
             status = "success",
             message = response.Message
-        });
-    }
-
-    [HttpPost("login")]
-    public async Task<IActionResult> Login(UserLoginRequestDto dto)
-    {
-        var response = await _userRepo.UserLoginAsync(dto);
-
-        if (!response.Flag)
-        {
-            return BadRequest(new { message = response.Message });
         }
-
-        return Ok(new
-        {
-            status="success",
-            message = response.Message,
-            data = new { accessToken = response.token }
-        });
+        );
     }
 }
