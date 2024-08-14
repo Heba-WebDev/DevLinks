f37b57b34a01d9ee125dd2ca3fecfe88c9a6e632
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-14 18:04:56 +0300

    fix: namespace

--- server/Application/Dtos/Auth/UserLoginResponseDto.cs
+++ server/Application/Dtos/Auth/UserLoginResponseDto.cs
@@ -1,2 +1,5 @@
-namespace Application.Auth.Dtos;
-public record UserLoginResponse(bool Flag, string Message = null!, string token = null!);
+
+using Application.Dtos.User;
+namespace Application.Dtos.Auth;
+
+public record UserLoginResponse(bool Flag, UserResponse? User, string Message = null!, string Token = null!);
