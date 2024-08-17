9e4f2259800e04051c9404701bdc795bfd90f1f0
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-17 13:43:23 +0300

    feat: get-all platforms

--- server/Application/Contracts/IPlatform.cs
+++ server/Application/Contracts/IPlatform.cs
@@ -7,4 +7,6 @@ public interface IPlatform
 {
     Task<CreatePlatformResponse> CreatePlatformAsync(CreatePlatformRequestDto dto);
     Task<UpdatePlatformResponse> UpdatePlatformAsync(string id, UpdatePlatformRequestDto dto);
+
+    Task<GetAllPlatformsResponse> GetAllPlatformsAsync();
 }
