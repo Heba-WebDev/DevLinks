cb051125b64ad55b0e522b991ad6e2a9dfcc9eca
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-18 12:27:18 +0300

    feat: react router setup

---
+++ client/src/routers/index.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default function Router() {
+    return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+}
