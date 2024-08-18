3910c5e8ea1698627955b68c1d808341fa6a8342
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-18 12:40:30 +0300

    chore: shadcn initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "./components/ui/button"
 function App() {
 
   return (
     <>
-     <h1 className="text-2xl text-orange-700">Hola Mundo</h1>
+      <h1 className="text-2xl text-orange-700">Hola Mundo</h1>
+      <Button className=" bg-blue-200">Click me</Button>
     </>
-  )
+  );
 }
 
 export default App
