5f9ad4fad36658ead4dfc30897f62c638e285048
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-18 13:14:06 +0300

    feat: added redux main store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './routers/index.tsx'
+import { Provider } from 'react-redux'
 import './index.css'
+import { store } from './store/index.ts'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </StrictMode>
+);
