a1ded54c8342413e9a7affc335d4ac9f2987f65e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-19 16:08:01 +0300

    feat: get a platform by id

--- server/Application/Contracts/IPlatform.cs
+++ server/Application/Contracts/IPlatform.cs
@@ -8,4 +8,5 @@ public interface IPlatform
     Task<CreatePlatformResponse> CreatePlatformAsync(CreatePlatformRequestDto dto);
     Task<UpdatePlatformResponse> UpdatePlatformAsync(string id, UpdatePlatformRequestDto dto);
     Task<GetAllPlatformsResponse> GetAllPlatformsAsync(bool? isSupported = null);
+    Task<GetAPlatformResponseDto> GetAPlatformAsyn(string id);
 }
--- server/Application/Dtos/Platform/GetAPlatformRequestDto.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace Application.Dtos.Platform;
-
-public class GetAPlatformRequestDto
-{
-    [Required(ErrorMessage = "A platform id is requried")]
-    public required Guid Id { get; set; }
-}
