8bd3bf60639f894f5fe2fbe9cb5b0e86011fc7c9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-19 16:26:47 +0300

    fix: typo

--- server/Application/Contracts/IPlatform.cs
+++ server/Application/Contracts/IPlatform.cs
@@ -8,5 +8,5 @@ public interface IPlatform
     Task<CreatePlatformResponse> CreatePlatformAsync(CreatePlatformRequestDto dto);
     Task<UpdatePlatformResponse> UpdatePlatformAsync(string id, UpdatePlatformRequestDto dto);
     Task<GetAllPlatformsResponse> GetAllPlatformsAsync(bool? isSupported = null);
-    Task<GetAPlatformResponseDto> GetAPlatformAsyn(string id);
+    Task<GetAPlatformResponseDto> GetAPlatformAsync(string id);
 }
