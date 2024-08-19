22260b667ec30ba21a8eb093a864e2e764fc854c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-19 12:52:21 +0300

    feat: signup route

--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App';
+import Register from '@/auth/signup/page';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/signup",
+    element: <Register />
+  }
 ]);
 
 export default function Router() {
