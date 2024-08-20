4e4b096a3257da41d87c378ab0cd93a0af8fffbd
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 19:24:26 +0300

    feat: update dto to reflect changes in the db

--- server/Application/Dtos/Platform/CreatePlatformRequestDto.cs
+++ server/Application/Dtos/Platform/CreatePlatformRequestDto.cs
@@ -10,6 +10,9 @@ public class CreatePlatformRequestDto
     [Required(ErrorMessage = "A platform base url is requried")]
     public required string BaseUrl { get; set; }
 
-    [Required(ErrorMessage = "A platform image url is requried")]
+    [Required(ErrorMessage = "A platform image is requried")]
     public IFormFile? Image { get; set; }
+
+    [Required(ErrorMessage ="A platform Color is required")]
+    public required string Color { get; set; }
 }
--- server/Application/Dtos/Platform/UpdatePlatformRequestDto.cs
+++ server/Application/Dtos/Platform/UpdatePlatformRequestDto.cs
@@ -6,5 +6,6 @@ public class UpdatePlatformRequestDto
     public string? Name { get; set; }
     public string? BaseUrl { get; set; }
     public IFormFile? Image { get; set; }
+    public string? Color { get; set; }
     public bool? IsSupported { get; set; }
 }
