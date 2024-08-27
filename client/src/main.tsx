7118c56988307c16df65763c8449fb085e44ed51
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-27 15:14:28 +0300

    feat: toastify and react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './routers/index.tsx'
 import { Provider } from 'react-redux'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './index.css'
 import { store } from './store/index.ts'
+import ReactQueryProvider from './util/react-query.provider.tsx';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router />
+      <ReactQueryProvider>
+        <Router />
+        <ToastContainer />
+      </ReactQueryProvider>
     </Provider>
   </StrictMode>
 );
