a5290b320cc64705be66ac6ec9b35413cedce0ba
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-04 18:41:11 +0300

    chore: remove access token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <h1 className="text-2xl text-orange-700">Hola ${user.firstName} ${user.lastName}</h1>
-      <h2>{user.accessToken} || no token</h2>
       <Button className=" bg-blue-200">Click me</Button>
     </>
   );
