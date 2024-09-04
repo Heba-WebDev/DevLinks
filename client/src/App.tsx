82c2c5e5ddcb9777f27580b298371bb1a1080bf5
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-04 15:49:12 +0300

    chore: checking user state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from "./components/ui/button"
-function App() {
+import { Button } from "./components/ui/button";
+import { useAppSelector } from "./hooks/store";
 
+function App() {
+  const user = useAppSelector((state) => state.user);
   return (
     <>
-      <h1 className="text-2xl text-orange-700">Hola Mundo</h1>
+      <h1 className="text-2xl text-orange-700">Hola ${user.firstName} ${user.lastName}</h1>
+      <h2>{user.accessToken} || no token</h2>
       <Button className=" bg-blue-200">Click me</Button>
     </>
   );
