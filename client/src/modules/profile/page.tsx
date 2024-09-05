6daa6f167bb1abdbf6c57bca4eb6a8d55ddb1974
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-05 13:18:52 +0300

    feat: profile main page

---
+++ client/src/modules/profile/page.tsx
@@ -0,0 +1,9 @@
+import AppLayout from "@/layouts/app-layout"
+
+export default function Profile() {
+    return (
+        <AppLayout>
+          <h1>Profile page</h1>
+        </AppLayout>
+    );
+}
