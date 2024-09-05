1f451a6e46b1fbaee919b34c05dfc43b19fdf382
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-05 13:17:12 +0300

    feat: removed auth to modules

--- client/src/auth/signin/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-import { loginrSchema } from "../schemas/login-schema";
-
-export type loginSchemaType = z.infer<typeof loginrSchema>;
--- client/src/auth/signup/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-import { registerSchema } from "../schemas/register-schema";
-
-export type registerSchemaType = z.infer<typeof registerSchema>;
