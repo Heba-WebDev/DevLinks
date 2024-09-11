b4f093246f4e3dfb9b0a470511ff51f735f84764
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-11 13:00:51 +0300

    chore: added userId to AddLinkAsync interface

--- server/Application/Contracts/ILink.cs
+++ server/Application/Contracts/ILink.cs
@@ -3,7 +3,7 @@ namespace Application.Contracts;
 
 public interface ILink
 {
-    Task<AddLinkResponseDto> AddLinkAsync(AddLinkRequestDto dto);
+    Task<AddLinkResponseDto> AddLinkAsync(Guid userId, AddLinkRequestDto dto);
     Task<UpdateLinkResponse> UpdateLinkAsync(UpdateLinkRequestDto dto);
     Task<GetAllLinksResponse> GetAllLinksAsync(Guid id);
     Task<DeleteLinkResponseDto> DeleteLinkAsync(Guid userId, Guid id);
