63408634def07769fb2675f3aa0de782500fca75
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-11 13:14:33 +0300

    chore: updated request dto

--- server/Application/Contracts/ILink.cs
+++ server/Application/Contracts/ILink.cs
@@ -4,7 +4,7 @@ namespace Application.Contracts;
 public interface ILink
 {
     Task<AddLinkResponseDto> AddLinkAsync(Guid userId, AddLinkRequestDto dto);
-    Task<UpdateLinkResponse> UpdateLinkAsync(UpdateLinkRequestDto dto);
+    Task<UpdateLinkResponse> UpdateLinkAsync(Guid userId, UpdateLinkRequestDto dto);
     Task<GetAllLinksResponse> GetAllLinksAsync(Guid id);
     Task<DeleteLinkResponseDto> DeleteLinkAsync(Guid userId, Guid id);
 }
