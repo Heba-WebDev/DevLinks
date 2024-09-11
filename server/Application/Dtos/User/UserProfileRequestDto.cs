7e14edebcea551ea6025c891c744077cc8e0df3b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-11 13:57:41 +0300

    fix: removed user id

--- server/Application/Dtos/User/UserProfileRequestDto.cs
+++ server/Application/Dtos/User/UserProfileRequestDto.cs
@@ -2,8 +2,6 @@ using System.ComponentModel.DataAnnotations;
 namespace Application.Dtos.User;
 public class UserProfileRequestDto
 {
-    [Required(ErrorMessage = "a user id is required")]
-    public required string Id { get; set; }
     [StringLength(20, ErrorMessage = "First name cannot exceed 20 characters")]
     public string? FirstName { get; set; }
 
