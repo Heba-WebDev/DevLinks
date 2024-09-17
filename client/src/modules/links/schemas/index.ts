dbff1900e9c4a6c5e9ebb72299c0fd26b3ef5276
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-18 01:01:51 +0300

    feat: updated links types and schemas

--- client/src/modules/links/schemas/index.ts
+++ client/src/modules/links/schemas/index.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
 export const linkSchema = z.object({
+    id: z.string().nullable(),
+    platform: z.string().nullable(),
     link: z.string().url({message: "A valid link is required"}),
 });
