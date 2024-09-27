b833f2174b301344a3f58272b1d1cadc1f5f5b18
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-27 23:18:46 +0300

    feat: pass a number of empty links

--- client/src/modules/links/components/empty-links-skeleton.tsx
+++ client/src/modules/links/components/empty-links-skeleton.tsx
@@ -1,21 +1,16 @@
 import { Skeleton } from "@/components/ui";
 
-export default function EmptyLinksSkeleton() {
+export default function EmptyLinksSkeleton({ number }: { number: number }) {
     return (
       <>
-        <div className="absolute inset-0 right-0 top-[50%] h-11 w-48 xl:w-52 mx-auto flex justify-center">
-          <Skeleton className="rounded-small w-48  xl:w-52" />
-        </div>
-
-        <div className="absolute inset-0 right-0 top-[60%] h-11 w-48 xl:w-52 mx-auto flex justify-center">
-          <Skeleton className="rounded-small w-48 xl:w-52" />
-        </div>
-        <div className="absolute inset-0 right-0 top-[70%] h-11 w-48 xl:w-52 mx-auto flex justify-center">
-          <Skeleton className="rounded-small w-48 xl:w-52" />
-        </div>
-        <div className="absolute inset-0 right-0 top-[80%] h-11 w-48 xl:w-52 mx-auto flex justify-center">
-          <Skeleton className="rounded-small w-48 xl:w-52" />
-        </div>
+        {Array.from({ length: number }).map((_, index) => (
+          <div
+            key={index}
+            className="h-11 w-48 xl:w-52 mx-auto flex justify-center"
+          >
+            <Skeleton className="rounded-small w-48 xl:w-52" />
+          </div>
+        ))}
       </>
     );
 }
